b0b1b36bf0f28c83b07efcae193d9e80d58ee83f
Ivan <ivanvolodin960@gmail.com>
2023-05-18 15:12:45 +0300

    fix: fix mistakes in component navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Navigation() {
   return (
     <nav className="flex justify-between px-5 h-[50px] bg-gray-200 items-center shadow-md">
-      Airport
-
+      <Link to="/">Airport</Link>
       <Link to="/auth">Authentication</Link>
     </nav>
   )
