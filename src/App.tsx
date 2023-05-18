c54725c11b8f81b6f811eca48efdb6fede77fc66
Ivan <ivanvolodin960@gmail.com>
2023-05-18 14:42:45 +0300

    feat: create pages and make routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import AirportDetailPage from './pages/AirportDetailPage';
+import AuthPage from './pages/AuthPage';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path='/' element={ }></Route>
+        <Route path='/' element={<MainPage />}></Route>
+        <Route path='/auth' element={<AuthPage />}></Route>
+        <Route path='/airport/:id' element={<AirportDetailPage />}></Route>
       </Routes>
     </div>
   );
