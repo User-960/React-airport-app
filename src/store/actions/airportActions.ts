7b4250fa597ae8b5562df73d43583de9fb9d524e
Ivan <ivanvolodin960@gmail.com>
2023-05-19 13:54:04 +0300

    feat: create function fetchAirports for fetch

---
+++ src/store/actions/airportActions.ts
@@ -0,0 +1,11 @@
+import { Dispatch } from "@reduxjs/toolkit";
+
+export const fetchAirports = () => {
+  return (dispatch: Dispatch) => {
+    try {
+      
+    } catch (e) {
+      
+    }
+  }
+};
