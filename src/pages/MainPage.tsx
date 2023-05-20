842da05b4cda5365514886eb33a6df81f20f57ad
Ivan <ivanvolodin960@gmail.com>
2023-05-20 14:28:02 +0300

    feat: create mechanism for work with server and create data base

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AirportCard from '../components/AirportCard';
 import AirportFilter from '../components/AirportFilter';
 import AirportSearch from '../components/AirportSearch';
+import { useAppDispatch } from '../hook/redux';
+import { fetchAirports } from '../store/actions/airportActions';
 
 export default function MainPage() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAirports());
+  }, [])
+
   return (
     <div className="container mx-auto max-w-[760px] pt-5">
       <AirportSearch />
--- src/store/actions/airportActions.ts
+++ src/store/actions/airportActions.ts
@@ -1,11 +1,16 @@
-import { Dispatch } from "@reduxjs/toolkit";
+import { AppDispatch } from "..";
+import axios from "../../axios";
+import { IAirport, ServerResponse } from "../../models/models";
+import { fetchError, fetching, fetchSuccess } from "../slices/airportSlice";
 
 export const fetchAirports = () => {
-  return (dispatch: Dispatch) => {
+  return async (dispatch: AppDispatch) => {
     try {
-      
+      dispatch(fetching);
+      const response = await axios.get<ServerResponse<IAirport>>('airports');
+      dispatch(fetchSuccess(response.data.results));
     } catch (e) {
-      
+      dispatch(fetchError(e as Error));
     }
   }
 };
