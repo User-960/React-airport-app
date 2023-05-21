a1e2364778fce68bec78b1fee2ab945533938224
Ivan <ivanvolodin960@gmail.com>
2023-05-22 01:36:25 +0300

    feat: add styles for component AirportCard

--- src/components/AirportCard.tsx
+++ src/components/AirportCard.tsx
@@ -7,8 +7,13 @@ interface AirportCardProps {
 
 export default function AirportCard({ airport }: AirportCardProps) {
   return (
-    <div>
-      {airport.name}
+    <div className="border rounded-md py-4 px-6 mb-2 cursor-pointer hover:shadow-md hover:transition-all">
+      <p className='text-lg font-bold'>{airport.name}</p>
+      <p>{airport?.region}</p>
+      <p>{airport?.type}</p>
+      <p>{airport?.country}</p>
+      <p>{airport?.local_code}</p>
+      <p>{airport?.ident}</p>
     </div>
   )
 };
