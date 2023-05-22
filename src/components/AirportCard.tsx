e833093c0ccbd04b650c908cb052cf78dc7401a9
Ivan <ivanvolodin960@gmail.com>
2023-05-22 13:40:45 +0300

    feat: add logics for transition to page AirportDetailPage

--- src/components/AirportCard.tsx
+++ src/components/AirportCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { IAirport } from '../models/models';
 
 interface AirportCardProps {
@@ -6,8 +7,15 @@ interface AirportCardProps {
 };
 
 export default function AirportCard({ airport }: AirportCardProps) {
+  const navigate = useNavigate();
+
+  const clickHandler = () => navigate(`/airport/${airport.id}`);
+
   return (
-    <div className="border rounded-md py-4 px-6 mb-2 cursor-pointer hover:shadow-md hover:transition-all">
+    <div
+      className="border rounded-md py-4 px-6 mb-2 cursor-pointer hover:shadow-md hover:transition-all"
+      onClick={clickHandler}
+    >
       <p className='text-lg font-bold'>{airport.name}</p>
       <p>{airport?.region}</p>
       <p>{airport?.type}</p>
--- src/pages/AirportDetailPage.tsx
+++ src/pages/AirportDetailPage.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function AirportDetailPage() {
+  const params = useParams<'id'>();
+
   return (
-    <div>
-      Detail
+    <div className='container mx-auto pt-5 max-w-[760px]'>
+      <h1>Airport {params.id}</h1>
     </div>
   )
 };
