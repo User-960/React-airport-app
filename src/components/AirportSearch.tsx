059c245f34052e883eb79d00fee1517862a6891b
Ivan <ivanvolodin960@gmail.com>
2023-05-24 00:10:56 +0300

    feat: create custom hook for input

--- src/components/AirportSearch.tsx
+++ src/components/AirportSearch.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useInput } from '../hook/input';
 
 export default function AirportSearch() {
+  const input = useInput('');
+
   return (
     <div className='mb-4'>
       <input
         type="text"
         className='border py-2 px-4 outline-0 w-full'
         placeholder='Type something here...'
+        {...input}
       />
     </div>
   )
