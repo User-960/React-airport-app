ace4977faae8e0c90c15342e08673eeb745c9992
Ivan <ivanvolodin960@gmail.com>
2023-05-24 01:06:45 +0300

    feat: create custom hook useDebounce for check input

--- src/components/AirportSearch.tsx
+++ src/components/AirportSearch.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDebounce } from '../hook/debounce';
 import { useInput } from '../hook/input';
 
 export default function AirportSearch() {
   const input = useInput('');
 
+  const debounced = useDebounce<string>(input.value, 400);
+
+  useEffect(() => {
+    if (debounced.length > 3) { }
+    console.log("debounced", debounced);
+  }, [debounced]);
+
   return (
-    <div className='mb-4'>
+    <div className='mb-4 relative'>
       <input
         type="text"
-        className='border py-2 px-4 outline-0 w-full'
+        className='border py-2 px-4 outline-0 w-full h-[42px]'
         placeholder='Type something here...'
         {...input}
       />
+
+      {/* <div className='absolute left-0 right-0 top-42 h-[200px] shadow-md'></div> */}
     </div>
   )
 };
