8ad155361233d6324c9eeb4926a0012b2b1aa9a0
Ivan <ivanvolodin960@gmail.com>
2023-05-25 00:16:11 +0300

    feat: add add jsx in component AirportFilter and styles

--- src/components/AirportFilter.tsx
+++ src/components/AirportFilter.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useAppSelector } from '../hook/redux';
 
 export default function AirportFilter() {
+  const { types, regions, countries, loading } = useAppSelector(state => state.handbook);
+
+  if (loading) return <p className='text-center'>Loading...</p>
+
   return (
-    <div>
-      Filter
+    <div className='border py-2 px-4 mb-2'>
+      <span className='font-bold mr-2'>Filter</span>
+
+      <select name="types" className='mr-2 border py-1 px-2'>
+        {types.map(t => <option key={t}>{t}</option>)}
+      </select>
+
+      <select name="countries" className='mr-2 border py-1 px-2'>
+        {countries.map(c => <option key={c}>{c}</option>)}
+      </select>
+
+      <select name="regions" className='border py-1 px-2'>
+        {regions.map(r => <option key={r}>{r}</option>)}
+      </select>
     </div>
   )
 };
