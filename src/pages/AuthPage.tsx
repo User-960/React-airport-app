b7299d9a5befa534e36810bd832882ce83d3626e
Ivan <ivanvolodin960@gmail.com>
2023-05-25 23:51:03 +0300

    feat: add jsx and logics on page AuthPage

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import { useInput } from '../hook/input';
 
 export default function AuthPage() {
+  const username = useInput('');
+  const password = useInput('');
+
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      Auth
-    </div>
+    <form
+      className="container mx-auto max-w-[500px] pt-8"
+      onSubmit={submitHandler}
+    >
+      <div className="mb-2">
+        <label className="block" htmlFor="username">Username</label>
+        <input className="border py-1 px-2 w-full" type="text" {...username} id="username" />
+      </div>
+
+      <div className="">
+        <label className="block" htmlFor="password">Password</label>
+        <input className="border py-1 px-2 w-full" type="password" {...password} id="password" />
+      </div>
+    </form>
   )
 };
